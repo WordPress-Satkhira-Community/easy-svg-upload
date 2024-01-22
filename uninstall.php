cbfcfb392676583bd1c5383d15e0bf3bc3530a9d
wpdelower <delowerjes@gmail.com>
2024-01-22 09:56:16 +0600

    Cleaned uninstall.php code

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * @link       https://wpsatkhira.com
- * @since      1.0
- *
- * @package    Easy_SVG_Uploads
- */
-
 // If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
